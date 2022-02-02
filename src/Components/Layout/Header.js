a3ff57b972ab82d97af5ac6d201eb7ae60a97d2c
elias <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2022-02-02 06:56:24 -0700

    first commit

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../images/meals-table.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="table with delicious meals"></img>
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
