7e8e3f99dc268429d225d9309ab3135a25b3bfa5
elias <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2022-02-10 06:47:29 -0700

    thursday from home - numOfCartItems

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
 import CartLogo from "./CartLogo";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../Store/Cart-context";
+import { useContext } from "react";
+import CartContext from "../Store/Cart-context";
 
 const HeaderCartButton = (props) => {
+  const CartCtx = useContext(CartContext);
+  const numOfCartItems = CartCtx.items.reduce((carNumber, item) => {
+    return carNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartLogo />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numOfCartItems}</span>
     </button>
   );
 };
